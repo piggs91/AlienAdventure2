f40c0d1b5a7e407d86ddadb8cbaa51ed59da7348
piggs91 <rpsr15@live.in>
2015-12-16 10:11:18 +0530

    add item form planet completed and updated

--- Alien Adventure/ItemsFromPlanet.swift
+++ Alien Adventure/ItemsFromPlanet.swift
@@ -9,9 +9,31 @@
 extension Hero {
     
     func itemsFromPlanet(inventory: [UDItem], planet: String) -> [UDItem] {
-        return [UDItem]()
+        var items = [UDItem]()
+        
+        for item in inventory{
+            
+            
+            guard let originPlanet = item.historicalData["PlanetOfOrigin"] as? String
+                else
+            {
+                //  print("no such key ")
+                return items // return the items
+            }
+            
+            if (originPlanet == "Glinda")
+            {
+                //  print(item.name)
+                // print(item.historicalData)
+                items.append(item)
+            }
+        }
+        
+        return items
     }
     
 }
+    
+
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 1"
