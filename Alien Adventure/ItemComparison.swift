729a689ded194b03255422982e42a0e54e4fec58
piggs91 <rpsr15@live.in>
2015-12-16 12:07:44 +0530

    item comparison done

--- Alien Adventure/ItemComparison.swift
+++ Alien Adventure/ItemComparison.swift
@@ -9,6 +9,19 @@
 import Foundation
 
 func <(lhs: UDItem, rhs: UDItem) -> Bool {
+
+    if ( lhs.rarity.rawValue > rhs.rarity.rawValue)
+    {
+        return false
+        }
+    else if (lhs.rarity.rawValue == rhs.rarity.rawValue)
+    {
+        
+        if ( lhs.baseValue > rhs.baseValue)
+        {
+            return false
+        }
+    }
     return true
 }
 
