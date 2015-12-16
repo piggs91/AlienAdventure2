6909b5d77300f57bb197add3a77cb8a317ca6da9
piggs91 <rpsr15@live.in>
2015-12-16 11:52:42 +0530

    find rarity of items

--- Alien Adventure/RarityOfItems.swift
+++ Alien Adventure/RarityOfItems.swift
@@ -9,7 +9,30 @@
 extension Hero {
     
     func rarityOfItems(inventory: [UDItem]) -> [UDItemRarity:Int] {
-        return [UDItemRarity:Int]()
+        var rarityList : [UDItemRarity:Int] = [ .Common: 0, .Uncommon: 0, .Rare: 0, .Legendary: 0 ] ;
+        
+        for item in inventory{
+           // print("item name \(item.name) \(item.rarity)")
+          switch item.rarity
+          {
+          case .Common:
+            
+        //    print("item name \(item.name) \(item.rarity)")
+          rarityList[.Common] = rarityList[.Common]! + 1
+         
+          case .Legendary:
+       //     print("item name \(item.name) \(item.rarity)")
+            rarityList[.Legendary] = rarityList[.Legendary]! + 1
+          case .Rare:
+         //   print("item name \(item.name) \(item.rarity)")
+            rarityList[.Rare] = rarityList[.Rare]! + 1
+          case .Uncommon:
+         //   print("item name \(item.name) \(item.rarity)")
+            rarityList[.Uncommon] = rarityList[.Uncommon]! + 1
+            }
+            
+        }
+        return rarityList
     }
 }
 
