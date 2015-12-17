d9be848d929e7042a0f361c20afeec391b536642
piggs91 <rpsr15@live.in>
2015-12-17 10:50:21 +0530

    completed json planet data

--- Alien Adventure/PlanetData.swift
+++ Alien Adventure/PlanetData.swift
@@ -8,9 +8,58 @@
 import Foundation
 
 extension Hero {
+    // enumeration to store the scores according to rarity of items
+    enum Points:Int
+    {
+        case Common = 1 , Uncommon , Rare , Legendary
+    }
+    
+   // function to calculate score
+    func calculateScore(Common : Int , Legendary : Int, Uncommon : Int , Rare : Int) -> Int
+        {
+            return ( Common * Points.Common.rawValue ) + ( Legendary * Points.Legendary.rawValue ) + ( Rare * Points.Rare.rawValue ) + ( Uncommon * Points.Uncommon.rawValue)
+        }
+    
     
     func planetData(dataFile: String) -> String {
+        
+        var mostIntriguingPlanet = (planetName : "", score : 0)
+      
+        
+        guard let urlForFile = NSBundle.mainBundle().URLForResource("PlanetData", withExtension: "json")
+        else
+        {
+        print("file not found")
+         return ""
+        }
+        guard let rawPlanetData = NSData(contentsOfURL: urlForFile)
+        else
+        {
+            print("cannot convert to raw data")
+            return ""
+        }
+    
+     guard let planetData = try! NSJSONSerialization.JSONObjectWithData(rawPlanetData, options: NSJSONReadingOptions()) as? [[String:AnyObject]]
+        else
+     {
+        print("cannot convert to ")
         return ""
+        }
+    for item in planetData
+    {
+      
+      let score = calculateScore(item["CommonItemsDetected"] as! Int, Legendary: item["LegendaryItemsDetected"] as! Int, Uncommon: item["UncommonItemsDetected"] as! Int, Rare: item["RareItemsDetected"] as! Int)
+    
+        if mostIntriguingPlanet.score <= score
+        {
+            mostIntriguingPlanet.score = score
+            mostIntriguingPlanet.planetName = item["Name"] as! String
+        }
+        
+        
+        }
+        
+           return mostIntriguingPlanet.planetName
     }
 }
 
