1b0630bdc081dbd29a066a23a774b202c10e43c9
piggs91 <rpsr15@live.in>
2015-12-17 12:50:51 +0530

    completer most common character

--- Alien Adventure/MostCommonCharacter.swift
+++ Alien Adventure/MostCommonCharacter.swift
@@ -7,8 +7,57 @@
 //
 
 extension Hero {
+  
+    //function to return max repeating characte in a string
+    func smartSort(var str:String) -> Character?
+    {   str=String(str.characters.sort())
+        
+        var currentCount = 0
+        var maxCount = 0
+        var currentCharacter = str.characters.first
+        var maxCharacter = str.characters.first
+        
+        for char in str.characters
+        {
+            // change current character if different
+            if ( currentCharacter != char)
+            {
+                currentCharacter = char
+                currentCount = 1
+                
+            }
+                
+            else
+            {
+                currentCount++
+            }
+            
+            
+            if ( currentCount >= maxCount)
+            {
+                maxCount = currentCount
+                maxCharacter = currentCharacter
+            }
+            
+            
+        }
+        return maxCharacter
+        
+    }
     
     func mostCommonCharacter(inventory: [UDItem]) -> Character? {
-        return nil
+        var allNames = ""
+        
+       // combine all to a single string
+        for item in inventory
+        {
+           
+           // print(item.name)
+            allNames += item.name.lowercaseString
+            
+        }
+       
+        
+        return smartSort(allNames)
     }
 }
